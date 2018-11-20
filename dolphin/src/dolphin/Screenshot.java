d1f3928b04d1056f88c448c7e426c4b0f8048e50
Lucas Didek <31461517+Rakanamatata@users.noreply.github.com>
2018-11-20 15:44:49 -0200

    Add files via upload

--- dolphin/src/dolphin/Screenshot.java
+++ dolphin/src/dolphin/Screenshot.java
@@ -4,16 +4,19 @@ package dolphin;
 import java.awt.*;
 import java.io.*;
 import javax.imageio.ImageIO;
+import DirectRobot;
 
 public class Screenshot implements RobotAction {
 
+
     public Object execute(Robot robot) throws IOException {
-        Toolkit defaultToolkit = Toolkit.getDefaultToolkit();
-        Rectangle shotArea = new Rectangle(defaultToolkit.getScreenSize());
+        ColorModel model = new DirectColorModel(32, 0xff0000, 0xff00, 0xff, 0xff000000);
+        BufferedImage image = new BufferedImage(model, Raster.createWritableRaster(model.createCompatibleSampleModel(width, height), new DataBufferInt(pixels, width * height), null), false, new Hashtable<Object, Object>());
         ByteArrayOutputStream bout = new ByteArrayOutputStream();
-        ImageIO.write(robot.createScreenCapture(shotArea), "jpg", bout);
-        
-//        JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(bout);;
+        ImageIO.write(image, "jpg", bout);
+
+        // @deprecated
+//        JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(bout);
 //        encoder.encode(robot.createScreenCapture(shotArea));
 //        
         return bout.toByteArray();  
